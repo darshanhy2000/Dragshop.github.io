08bbc24f901008c9260a30d14a7e2e65099e333d
TechnoCodehub <106342557+darshanhy2000@users.noreply.github.com>
2023-12-29 19:00:05 +0530

    Order placed alert

--- build/static/js/4145.7cf3a818.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[4145],{4145:(e,s,n)=>{n.r(s),n.d(s,{default:()=>a});var o=n(2791),r=n(4880),t=n(2007),c=n.n(t),p=n(184);const h=e=>{let{children:s,offset:n=0}=e;const t=(0,r.TH)();return(0,o.useEffect)((()=>{t.hash||window.scrollTo({top:n,behavior:"smooth"})}),[t,n]),(0,p.jsx)(p.Fragment,{children:s})};h.propTypes={children:c().node.isRequired,offset:c().number};const a=h}}]);
-//# sourceMappingURL=4145.7cf3a818.chunk.js.map
--- src/component/Checkout/Payment.js
+++ src/component/Checkout/Payment.js
@@ -1,83 +1,11 @@
-// import React from 'react';
-// import phonePeLogo from '../../assets/img/common/phonepe.png';
-// import GooglePay from '../../assets/img/common/Google-Pay.png';
-// import UpiImg from '../../assets/img/common/upi-logo.png';
-
-// const Payment = () => {
-//     return (
-//         <>
-//             <div className="order_review bg-white">
-//                 <div className="check-heading">
-//                     <h3>Payment</h3>
-//                 </div>
-//                 <div className="payment_method">
-//                     <form>
-//                         <div className="accordion" id="accordionExample">
-//                             <div className="payment_area_wrappers">
-//                                 <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-//                                     <div className="payment_body">
-//                                         <p>Direct Bank Transfer</p>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                             <div className="payment_area_wrappers">
-//                                 <div className="heading_payment" id="headingTwo">
-//                                     <div className="collapsed" data-toggle="collapse" data-target="#collapseTwo">
-//                                         <input type="radio" name="payment" id="javascript" value="JavaScript" />
-//                                         <label htmlFor="javascript">Mobile Banking</label>
-//                                     </div>
-//                                 </div>
-//                                 <div id="collapseTwo" className="collapse" data-parent="#accordionExample">
-//                                     <div className="payment_body">
-//                                         <p>Direct Mobile Transfer</p>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                             <div className="payment_area_wrappers">
-//                                 <div id="collapseThree" className="collapse" data-parent="#accordionExample">
-//                                     <div className="payment_body">
-//                                         <p>Some placeholder content for the PayPal payment option. This panel is hidden by default.</p>
-//                                     </div>
-//                                 </div>
-
-                            
-//                                 <div className="heading_payment" id="headingFour">
-//                                     <div className="collapsed" data-toggle="collapse" data-target="#collapseFour">
-//                                     <label htmlFor="Upi">
-//                                         <img src={UpiImg} alt="PhonePe" className="payment-logo" />
-//                                         </label>
-//                                         <label htmlFor="phonepe">
-//                                             <img src={phonePeLogo} alt="PhonePe" className="payment-logo" />
-//                                         </label>
-//                                         <label htmlFor="phonepe">
-//                                             <img src={GooglePay} alt="PhonePe" className="payment-logo" />
-//                                         </label>
-//                                     </div>
-//                                 </div>
-//                                 </div>
-                                
-//                         </div>
-//                     </form>
-//                 </div>
-//                 <button className="theme-btn-one btn-black-overlay btn_sm">Place Order</button>
-//             </div>
-            
-//         </>
-//     )
-// }
-
-// export default Payment
-
-
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import phonePeLogo from '../../assets/img/common/phonepe.png';
 import GooglePay from '../../assets/img/common/Google-Pay.png';
 import UpiImg from '../../assets/img/common/upi-logo.png';
 import Swal from 'sweetalert2';
 
-
 const Payment = () => {
-    const [setOrderPlaced] = useState(false);
+    const [orderPlaced, setOrderPlaced] = useState(false);
 
     const handlePlaceOrder = () => {
         // Perform any necessary actions when placing the order
@@ -85,15 +13,28 @@ const Payment = () => {
 
         // Update the state to show the "Thank You" message
         setOrderPlaced(true);
+
+        // Display SweetAlert notification
+        Swal.fire({
+            title: 'Thank you!',
+            text: 'Your order has been placed.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
     };
 
     // Clear the "Thank You" message after a delay
-    Swal.fire({
-        title: 'Thank you!',
-        text: 'Your order has been placed.',
-        icon: 'success',
-        confirmButtonText: 'OK'
-    });
+    useEffect(() => {
+        if (orderPlaced) {
+            const timeoutId = setTimeout(() => {
+                setOrderPlaced(false);
+            }, 5000); // Adjust the delay (in milliseconds) as needed
+
+            return () => clearTimeout(timeoutId);
+        }
+    }, [orderPlaced]);
+
+    
 
     return (
         <div className="order_review bg-white">
@@ -133,18 +74,19 @@ const Payment = () => {
 
                             
                                  <div className="heading_payment" id="headingFour">
-                                     <div className="collapsed" data-toggle="collapse" data-target="#collapseFour">
-                                     <label htmlFor="Upi">
-                                         <img src={UpiImg} alt="PhonePe" className="payment-logo" />
-                                         </label>
-                                         <label htmlFor="phonepe">
-                                             <img src={phonePeLogo} alt="PhonePe" className="payment-logo" />
-                                         </label>
-                                         <label htmlFor="phonepe">
-                                             <img src={GooglePay} alt="PhonePe" className="payment-logo" />
-                                         </label>
-                                     </div>
-                                 </div>
+                                <div className="collapsed" data-toggle="collapse" data-target="#collapseFour">
+                                    <a href="upi://pay?pa=example@upi&pn=Example%20Name&mc=1234&tid=9876543210&tr=1234567890&tn=Example%20Transaction&am=10.00&cu=INR&url=https://example.com/cb" target="_blank" rel="noopener noreferrer">
+                                        <img src={UpiImg} alt="UPI" className="payment-logo" />
+                                    </a>
+                                    <a href="https://www.phonepe.com/en/" target="_blank" rel="noopener noreferrer">
+                                        <img src={phonePeLogo} alt="PhonePe" className="payment-logo" />
+                                    </a>
+                                    <a href="https://pay.google.com/intl/en_in/about/" target="_blank" rel="noopener noreferrer">
+                                        <img src={GooglePay} alt="Google Pay" className="payment-logo" />
+                                    </a>
+                                </div>
+                            </div>
+
                                  </div>
                                 
                          </div>
@@ -157,8 +99,7 @@ const Payment = () => {
                 </div>
             
         </div>
-    );
-};
+    );}
 
 export default Payment;
 
